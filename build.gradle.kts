78b632dc13b5e0c7d38ee70a85013248f449654c
Anton <anton.wiblishauser@gmx.de>
2022-09-05 21:39:56 +0200

    update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,36 +2,150 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
-    id("fabric-loom") version "0.12-SNAPSHOT"
+    kotlin("plugin.serialization") version "1.7.10"
+    id("fabric-loom") version "1.0-SNAPSHOT"
     id("io.github.juuxel.loom-quiltflower") version "1.7.3"
     id("org.quiltmc.quilt-mappings-on-loom") version "4.2.1"
+
+    id("com.modrinth.minotaur") version "2.4.+"
+    id("com.github.breadmoirai.github-release") version "2.+"
+    `maven-publish`
+    signing
 }
 
 group = "dev.nyon"
 version = "1.0.0"
+val authors = listOf("btwonion")
+val githubRepo: String by project
 
 repositories {
     mavenCentral()
+    maven("https://maven.isxander.dev/releases")
+    maven("https://maven.terraformersmc.com")
 }
 
+val minecraftVersion: String by project
+val quiltMappingsVersion: String by project
+val fabricLoaderVersion: String by project
+val fabricAPIVersion: String by project
+val fabricLanguageKotlinVersion: String by project
+val configLibVersion: String by project
+val silkVersion: String by project
+
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.1")
+    minecraft("com.mojang:minecraft:$minecraftVersion")
     mappings(loom.layered {
-        addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:1.19.1+build.2:v2"))
+        addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:$quiltMappingsVersion"))
         officialMojangMappings()
     })
 
-    modImplementation("net.fabricmc:fabric-loader:0.14.8")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.58.5+1.19.1")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.2+kotlin.1.7.10")
+    modImplementation("net.fabricmc:fabric-loader:$fabricLoaderVersion")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricAPIVersion")
+    modImplementation("net.fabricmc:fabric-language-kotlin:$fabricLanguageKotlinVersion")
 
-    testImplementation(kotlin("test"))
+    modImplementation("net.silkmc:silk-core:$silkVersion")
+
+    modImplementation("dev.isxander:yet-another-config-lib:$configLibVersion")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    processResources {
+        val modId: String by project
+        val modName: String by project
+        val modDescription: String by project
+
+        inputs.property("id", modId)
+        inputs.property("group", project.group)
+        inputs.property("name", modName)
+        inputs.property("description", modDescription)
+        inputs.property("version", project.version)
+        inputs.property("github", githubRepo)
+
+        filesMatching(listOf("fabric.mod.json", "quilt.mod.json")) {
+            expand(
+                "id" to modId,
+                "group" to project.group,
+                "name" to modName,
+                "description" to modDescription,
+                "version" to project.version,
+                "github" to githubRepo,
+            )
+        }
+    }
+
+    register("releaseMod") {
+        group = "mod"
+
+        dependsOn("modrinth")
+        dependsOn("modrinthSyncBody")
+        dependsOn("publishModPublicationToReleasesRepository")
+        dependsOn("githubRelease")
+    }
+}
+val changelogText =
+    file("changelogs/${project.version}.md").takeIf { it.exists() }?.readText() ?: "No changelog provided."
+
+/*val modrinthId: String by project
+modrinth {
+    token.set(findProperty("modrinth.token")?.toString())
+    projectId.set(modrinthId)
+    versionNumber.set("${project.version}")
+    versionType.set("beta")
+    uploadFile.set(tasks["remapJar"])
+    gameVersions.set(listOf("1.19", "1.19.1", "1.19.2"))
+    loaders.set(listOf("fabric", "quilt"))
+    dependencies {
+        required.project("fabric-api")
+    }
+    changelog.set(changelogText)
+    syncBodyFrom.set(file("README.md").readText())
+}
+ */
+
+publishing {
+    publications {
+        register<MavenPublication>(project.name) {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name
+            this.version = rootProject.version.toString()
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+
+                developers {
+                    authors.forEach {
+                        developer {
+                            name.set(it)
+                        }
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License 3")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+                    }
+                }
+
+                url.set("https://github.com/${githubRepo}")
+
+                scm {
+                    connection.set("scm:git:git://github.com/${githubRepo}.git")
+                    url.set("https://github.com/${githubRepo}/tree/main")
+                }
+            }
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+    kotlinOptions.freeCompilerArgs += "-Xskip-prerelease-check"
+}
+
+signing {
+    sign(publishing.publications)
 }
