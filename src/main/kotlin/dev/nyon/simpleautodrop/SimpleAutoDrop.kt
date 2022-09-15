d6458ab40c7634cf86bfe2f55c3467312ca91f84
Anton <anton.wiblishauser@gmx.de>
2022-09-15 14:53:37 +0200

    finally fix auto drop

--- src/main/kotlin/dev/nyon/simpleautodrop/SimpleAutoDrop.kt
+++ src/main/kotlin/dev/nyon/simpleautodrop/SimpleAutoDrop.kt
@@ -6,17 +6,12 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.KeyMapping
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screens.inventory.InventoryScreen
-import net.minecraft.core.BlockPos
-import net.minecraft.core.Direction
-import net.minecraft.network.protocol.game.ServerboundPlayerActionPacket
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.inventory.ClickType
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.silkmc.silk.core.text.literalText
 import org.lwjgl.glfw.GLFW
-
 
 object SimpleAutoDrop {
 
@@ -50,14 +45,10 @@ object SimpleAutoDrop {
         val minecraft = Minecraft.getInstance()
         val player = minecraft.player ?: return
         val screen = InventoryScreen(player)
-        player.inventory?.items?.forEachIndexed { index, itemStack ->
-            if (itemStack.item == Items.AIR || !itemIds.contains(Item.getId(itemStack.item))) return@forEachIndexed
-            if (index <= 8) {
-                val action = ServerboundPlayerActionPacket.Action.DROP_ALL_ITEMS
-                player.inventory.removeItem(itemStack)
-                player.connection.send(ServerboundPlayerActionPacket(action, BlockPos.ZERO, Direction.DOWN))
-            } else minecraft.gameMode?.handleInventoryMouseClick(
-                screen.menu.containerId, index, 1, ClickType.THROW, player
+        screen.menu.slots.forEachIndexed { i, slot ->
+            if (slot.item.item == Items.AIR || !itemIds.contains(Item.getId(slot.item.item)) || !slot.hasItem()) return@forEachIndexed
+            minecraft.gameMode?.handleInventoryMouseClick(
+                screen.menu.containerId, i, 1, ClickType.THROW, player
             )
             player.swing(InteractionHand.MAIN_HAND)
         }
