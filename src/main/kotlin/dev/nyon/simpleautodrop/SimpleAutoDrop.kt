ed0b21644d40ab78ab2fb4045a5070f85aa9756a
Anton <anton.wiblishauser@gmx.de>
2023-04-20 19:16:46 +0200

    launch item dropping in mc scope

--- src/main/kotlin/dev/nyon/simpleautodrop/SimpleAutoDrop.kt
+++ src/main/kotlin/dev/nyon/simpleautodrop/SimpleAutoDrop.kt
@@ -3,10 +3,8 @@ package dev.nyon.simpleautodrop
 import com.mojang.blaze3d.platform.InputConstants
 import dev.nyon.simpleautodrop.config.*
 import dev.nyon.simpleautodrop.screen.ConfigScreen
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.KeyMapping
 import net.minecraft.client.Minecraft
@@ -19,9 +17,9 @@ import net.minecraft.world.item.Items
 import org.lwjgl.glfw.GLFW
 import kotlin.time.Duration.Companion.milliseconds
 
-val scope = CoroutineScope(Dispatchers.Default)
+lateinit var mcDispatcher: CoroutineDispatcher
 
-object SimpleAutoDrop {
+object SimpleAutoDrop : ClientModInitializer {
 
     private val toggleKeyBind = KeyBindingHelper.registerKeyBinding(
         KeyMapping(
@@ -58,21 +56,24 @@ object SimpleAutoDrop {
     }
 
     private var jobWaiting = false
-    fun onTake() {
-        if (!settings.enabled) return
-        if (settings.activeArchives.isEmpty()) return
+    fun onTake() = runBlocking {
+        if (!settings.enabled) return@runBlocking
+        if (settings.activeArchives.isEmpty()) return@runBlocking
         val minecraft = Minecraft.getInstance()
-        val player = minecraft.player ?: return
-        if (jobWaiting) return
+        val player = minecraft.player ?: return@runBlocking
+        if (jobWaiting) return@runBlocking
         jobWaiting = true
 
-        scope.launch {
+        launch(mcDispatcher) {
             delay(200.milliseconds)
 
             val screen = InventoryScreen(player)
             screen.menu.slots.forEachIndexed { _, slot ->
                 if (blockedSlots.contains(slot.index)) return@forEachIndexed
-                if (slot.item.item == Items.AIR || !itemIds.contains(BuiltInRegistries.ITEM.getKey(slot.item.item).toString()) || !slot.hasItem()) return@forEachIndexed
+                if (slot.item.item == Items.AIR || !itemIds.contains(
+                        BuiltInRegistries.ITEM.getKey(slot.item.item).toString()
+                    ) || !slot.hasItem()
+                ) return@forEachIndexed
                 minecraft.gameMode?.handleInventoryMouseClick(
                     screen.menu.containerId, slot.index, 1, ClickType.THROW, player
                 )
