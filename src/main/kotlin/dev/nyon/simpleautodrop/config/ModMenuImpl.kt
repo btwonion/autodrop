23450fe8fe3ebe486086fe12b11e0d26860ddffa
Anton <anton.wiblishauser@gmx.de>
2023-06-16 17:13:37 +0200

    fix modmenu offset resetting after configuring autodrop

--- src/main/kotlin/dev/nyon/simpleautodrop/config/ModMenuImpl.kt
+++ src/main/kotlin/dev/nyon/simpleautodrop/config/ModMenuImpl.kt
@@ -2,14 +2,13 @@ package dev.nyon.simpleautodrop.config
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
-import com.terraformersmc.modmenu.gui.ModsScreen
 import dev.nyon.simpleautodrop.screen.ConfigScreen
 
 @Suppress("unused")
 class ModMenuImpl : ModMenuApi {
     override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
         return ConfigScreenFactory {
-            ConfigScreen(ModsScreen(it))
+            ConfigScreen(it)
         }
     }
 }
