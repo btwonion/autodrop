92cc080f3c7253d97c641ffb77e8a6e29c942d3f
btwonion <btwonion@gmx.de>
2023-09-21 21:39:16 +0200

    remove old code

--- src/main/kotlin/dev/nyon/autodrop/util/Serializers.kt
+++ src/main/kotlin/dev/nyon/autodrop/util/Serializers.kt
@@ -7,19 +7,6 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.item.Item
-
-class ItemSerializer : KSerializer<Item> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("item", PrimitiveKind.INT)
-
-    override fun serialize(encoder: Encoder, value: Item) {
-        encoder.encodeInt(Item.getId(value))
-    }
-
-    override fun deserialize(decoder: Decoder): Item {
-        return Item.byId(decoder.decodeInt())
-    }
-}
 
 object ResourceLocationSerializer : KSerializer<ResourceLocation> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("resourcelocation", PrimitiveKind.STRING)
