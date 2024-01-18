736b51a93df7c551ae7beca19ddbef7ef121d84c
btwonion <btwonion@gmx.de>
2024-01-18 17:02:51 +0100

    bump version to 1.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "dev.nyon"
-val majorVersion = "1.6.1"
+val majorVersion = "1.6.2"
 val mcVersion = "1.20.4"
 version = "$majorVersion-$mcVersion"
 val authors = listOf("btwonion")
