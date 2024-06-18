ebbdd621e13b47fcf39f9360baaf255108216cdb
btwonion <btwonion@gmx.de>
2024-06-18 17:10:08 +0200

    remove parchment layer

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -10,7 +10,7 @@ import java.time.Instant
 plugins {
     id("dev.kikugie.stonecutter")
 }
-stonecutter active "1.20.1" /* [SC] DO NOT EDIT */
+stonecutter active "1.21" /* [SC] DO NOT EDIT */
 
 stonecutter registerChiseled tasks.register("buildAllVersions", stonecutter.chiseled) {
     group = "mod"
